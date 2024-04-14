21100d86a9d41e6458c1aeef66da3b853c29aacf
hasanalam24 <hasanalam365@gmail.com>
2024-04-15 02:11:36 +0600

    toggle show and hide pass

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -12,6 +12,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
+
     //create user
     const createAccount = (email, password) => {
         setLoading(true)
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -5,11 +5,16 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Helmet } from "react-helmet-async";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
 
 const Register = () => {
-
+    const [showPass, setShowPass] = useState(false)
     const [passError, setPassError] = useState()
     const { createAccount, updateProfileUser } = useContext(AuthContext)
+
+
     const {
         register,
         handleSubmit,
@@ -51,7 +56,7 @@ const Register = () => {
 
     }
 
-    // console.log(watch("example"))
+
 
     return (
         <div className="w-3/4 lg:w-1/3 mx-auto  bg-base-200">
@@ -97,10 +102,20 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered"
-                                {...register("password", { required: true })}
 
-                            />
+                            <div className="relative">
+                                <input type={showPass ? "text" : "password"} placeholder="password" className="input input-bordered w-full"
+                                    {...register("password", { required: true })}
+
+                                />
+                                <span className="absolute top-4 right-3" onClick={() => setShowPass(!showPass)}>
+
+                                    {
+                                        showPass ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>
+                                    }
+                                </span>
+                            </div>
+
                             {/* {errors.password && <span className='text-red-500'>This field is required</span>} */}
                             <span className="text-red-500">{passError}</span>
                             <label className="label">
