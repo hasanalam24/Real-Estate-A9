ebea74fe75fe431fbadb67bcbf85f9109ab65fb4
hasanalam24 <hasanalam365@gmail.com>
2024-04-16 02:12:29 +0600

    details section final design touch

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -8,20 +8,35 @@ const Details = () => {
     const loadData = useLoaderData()
     const view = loadData.find((data) => data.id == search)
     // console.log(view)
-    const { images, estate_title, segment_name, price, description, area, location, facilities, discount } = view
+    const { images, estate_title, price, description, area, location, facilities, discount, status } = view
 
     return (
         <div className="card lg:card-side bg-base-100 shadow-xl">
             <Helmet>
                 <title>Details</title>
             </Helmet>
-            <figure><img src={images} alt="Album" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{estate_title}</h2>
-                <p>{description}</p>
-                <p>{segment_name}</p>
+            <div>
+                <h1></h1>
+                <figure><img className="lg:w-[500px] lg:h-[500px] rounded-lg" src={images} alt="Album" /></figure>
             </div>
-        </div>
+            <div className="p-4 space-y-3">
+
+                <h2 className="card-title">{estate_title}</h2>
+                <p className="bg-orange-500 max-w-max px-4 font-semibold text-white rounded-full flex items-center justify-center">{status}</p>
+
+                <p><span className="font-medium">Deschiption:</span> {description}</p>
+                <p><span className="font-medium">Area:</span> {area}</p>
+                <p><span className="font-medium">Deschiption:</span> {location}</p>
+                <div>
+                    <h3 className="font-semibold">Facilities Here:</h3>
+                    {
+                        facilities.map((facility) => <li key={facility.id}> {facility}</li>)
+                    }
+                </div>
+                <p><span className="font-medium">Price:</span> {price}</p>
+                <p className="text-red-500"><span className="font-medium ">Discount:</span> <span className="line-through">{discount}</span></p>
+            </div>
+        </div >
     );
 };
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,12 +18,8 @@ const Navbar = () => {
                     <NavLink to='/profile'> Profile</NavLink>
                 </li>
             }
-            <li>
-                <NavLink to='/UpdateProfile'>Update Profile</NavLink>
-            </li>
-            <li>
-                <NavLink to='/register'>Register</NavLink>
-            </li>
+
+
             <li>
                 <NavLink to='/contact'>Contact</NavLink>
             </li>
